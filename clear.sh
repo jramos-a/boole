323e78b748d4e0e8d853486634b11b6535bafa65
jesus <minecraftgamingra@gmail.com>
2026-06-26 12:21:08 +0200

    helpers for eval

---
+++ clear.sh
@@ -0,0 +1,10 @@
+
+
+rm ex00/Adder
+rm ex01/Multiplier
+rm ex02/GrayCode
+rm ex03/BooleanEval
+rm ex04/TruthTable
+rm ex05/Negation
+rm ex06/CNF
+
---
+++ compile.sh
@@ -0,0 +1,14 @@
+
+cd ex00 && rustc main.rs -o Adder
+cd ..
+cd ex01 &&rustc main.rs -o Multiplier
+cd ..
+cd ex02 && rustc main.rs -o GrayCode
+cd ..
+cd ex03 && rustc main.rs -o BooleanEval
+cd ..
+cd ex04 && rustc main.rs -o TruthTable
+cd ..
+cd ex05 && rustc main.rs -o Negation
+cd ..
+cd ex06 && rustc main.rs -o CNF
